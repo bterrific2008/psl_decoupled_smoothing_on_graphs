9e2150b8de20b8889e50f347a82d6af06192d4f0
Bryan Tor <bterrific2008@gmail.com>
2020-07-01 14:24:55 -0700

    Add data generation to run_method. Fix line endings

--- run_method.sh
+++ run_method.sh
@@ -1,114 +1,147 @@
 #!/bin/bash
 # Describe what this does
 
-readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly BASE_OUT_DIR="${THIS_DIR}/results"
 
 readonly ADDITIONAL_PSL_OPTIONS='-D log4j.threshold=TRACE'
+readonly ADDITIONAL_LEARN_OPTIONS='--learn GaussianProcessPrior -D weightlearning.evaluator=RankingEvaluator -D rankingevaluator.representative=AUROC'
+readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval CategoricalEvaluator RankingEvaluator'
 
 # An identifier to differentiate the output of this script/experiment from other scripts.
 readonly RUN_ID='decoupled-smoothing'
 
+function generate_data() {
+  random_seed=$1
+  data_name=$2
+  train_test=$3
+
+  printf -v seed_nm "%04d" $random_seed
+  local logPath="${BASE_DATA_DIR}/${train_test}/${data_name}/01pct/${seed_nm}rand/data_log.json"
+
+  if [[ -e "${logPath}" ]]; then
+    echo "Output data already exists, skipping data generation"
+  elif [ "$train_test" = learn ]; then
+    echo "Generating data with seed ${random_seed} and data ${data_name} for ${train_test}"
+    python3 write_psl_data.py --seed ${random_seed} --data ${data_name}.mat --learn
+  else
+    echo "Generating data with seed ${random_seed} and data ${data_name} for ${train_test}"
+    python3 write_psl_data.py --seed ${random_seed} --data ${data_name}.mat
+  fi
+}
+
 function run_psl() {
-    local cliDir=$1
-    local outDir=$2
-    local extraOptions=$3
-    local data_nm=$4
-    local rand_sd=$5
-    local pct_lbl=$6
-    local learn_eval=$7
+  local cliDir=$1
+  local outDir=$2
+  local extraOptions=$3
+  local data_nm=$4
+  local rand_sd=$5
+  local pct_lbl=$6
+  local learn_eval=$7
 
-    mkdir -p "${outDir}"
+  mkdir -p "${outDir}"
 
-    local outPath="${outDir}/out${pct_lbl}${learn_eval}.txt"
-    local errPath="${outDir}/out${pct_lbl}${learn_eval}.err"
-    local timePath="${outDir}/time${pct_lbl}${learn_eval}.txt"
+  local outPath="${outDir}/out${pct_lbl}${learn_eval}.txt"
+  local errPath="${outDir}/out${pct_lbl}${learn_eval}.err"
+  local timePath="${outDir}/time${pct_lbl}${learn_eval}.txt"
 
-    if [[ -e "${outPath}" ]]; then
-        echo "Output file already exists, skipping: ${outPath}"
-        return 0
-    fi
+  if [[ -e "${outPath}" ]]; then
+    echo "Output file already exists, skipping: ${outPath}"
+    return 0
+  fi
 
-    if [ $learn_eval == 'learn' ]; then
-      pushd . > /dev/null
-          cd "${cliDir}"
+  if [ $learn_eval == 'learn' ]; then
+    pushd . >/dev/null
+    cd "${cliDir}"
 
-          # fix the data settings
-          sed "s/learn_eval/learn/g ; s/rand_sd/${rand_sd}rand/g ; s/pct_lbl/${pct_lbl}pct/g ; s/data_nm/${data_nm}/g" \
-          base.data > gender-learn.data
+    # fix the data settings
+    sed "s/learn_eval/learn/g ; s/rand_sd/${rand_sd}rand/g ; s/pct_lbl/${pct_lbl}pct/g ; s/data_nm/${data_nm}/g" \
+      base.data >gender-learn.data
 
-          # Run PSL.
-          /usr/bin/time -v --output="${timePath}" ./run-learn.sh ${extraOptions} > "${outPath}" 2> "${errPath}"
+    # Run PSL.
+    /usr/bin/time -v --output="${timePath}" ./run-learn.sh ${extraOptions} >"${outPath}" 2>"${errPath}"
 
-          # Copy any artifacts into the output directory.
-          cp -R inferred-predicates "${outDir}/inferred-predicates${pct_lbl}"
-          cp *.data "${outDir}/"
-          cp *.psl "${outDir}/"
-      popd > /dev/null
-    else
-      pushd . > /dev/null
-          cd "${cliDir}"
+    # Copy any artifacts into the output directory.
+    cp -R inferred-predicates "${outDir}/inferred-predicates${pct_lbl}"
+    cp *.data "${outDir}/"
+    cp *.psl "${outDir}/"
+    popd >/dev/null
+  else
+    pushd . >/dev/null
+    cd "${cliDir}"
 
-          # fix the data settings
-          sed "s/learn_eval/eval/g ; s/rand_sd/${rand_sd}rand/g ; s/pct_lbl/${pct_lbl}pct/g ; s/data_nm/${data_nm}/g" \
-          base.data > gender-eval.data
+    # fix the data settings
+    sed "s/learn_eval/eval/g ; s/rand_sd/${rand_sd}rand/g ; s/pct_lbl/${pct_lbl}pct/g ; s/data_nm/${data_nm}/g" \
+      base.data >gender-eval.data
 
-          # Run PSL.
-          /usr/bin/time -v --output="${timePath}" ./run-eval.sh ${extraOptions} > "${outPath}" 2> "${errPath}"
+    # Run PSL.
+    /usr/bin/time -v --output="${timePath}" ./run-eval.sh ${extraOptions} >"${outPath}" 2>"${errPath}"
 
-          # Copy any artifacts into the output directory.
-          cp -R inferred-predicates "${outDir}/inferred-predicates${pct_lbl}"
-          cp *.data "${outDir}/"
-          cp *.psl "${outDir}/"
-      popd > /dev/null
-    fi
+    # Copy any artifacts into the output directory.
+    cp -R inferred-predicates "${outDir}/inferred-predicates${pct_lbl}"
+    cp *.data "${outDir}/"
+    cp *.psl "${outDir}/"
+    popd >/dev/null
+  fi
 
 }
 
 function run_method() {
-    local exampleDir=$1
-    local data_nm=$2
-    local rand_sd=$3
-    local pct_lbl=$4
-    local learn_eval=$5
+  local exampleDir=$1
+  local data_nm=$2
+  local rand_sd=$3
+  local pct_lbl=$4
+  local learn_eval=$5
 
+  local exampleName=$(basename "${exampleDir}")
+  local cliDir="$exampleDir"
+  local outDir="${BASE_OUT_DIR}/${RUN_ID}/${learn_eval}/${exampleName}/${data_nm}/${rand_sd}"
 
-    local exampleName=`basename "${exampleDir}"`
-    local cliDir="$exampleDir"
-    local outDir="${BASE_OUT_DIR}/${RUN_ID}/${learn_eval}/${exampleName}/${data_nm}/${rand_sd}"
-    local options="${ADDITIONAL_PSL_OPTIONS}"
+  local options=""
+  case $learn_eval in
+    learn)
+      options = "${ADDITIONAL_LEARN_OPTIONS} ${ADDITIONAL_PSL_OPTIONS}"
+      ;;
+    eval)
+      options = "${ADDITIONAL_EVAL_OPTIONS} ${ADDITIONAL_PSL_OPTIONS}"
+      ;;
+    *)
+      options="${ADDITIONAL_PSL_OPTIONS}"
+      ;;
+  esac
 
-    echo "Running ${exampleName} -- ${RUN_ID}."
+  echo "Running ${exampleName} -- ${RUN_ID}."
 
-    run_psl "${cliDir}" "${outDir}" "${options}" "${data_nm}" "${rand_sd}" "${pct_lbl}" "${learn_eval}"
+  run_psl "${cliDir}" "${outDir}" "${options}" "${data_nm}" "${rand_sd}" "${pct_lbl}" "${learn_eval}"
 }
 
 function main() {
-    if [[ $# -eq 0 ]]; then
-        echo "USAGE: $0 <data> <random seed> <percent labeled> <train or test> <example dir> ..."
-        exit 1
-    fi
+  if [[ $# -eq 0 ]]; then
+    echo "USAGE: $0 <data> <random seed> <percent labeled> <train or test> <example dir> ..."
+    exit 1
+  fi
 
-    local data_nm=$1
-    shift
+  local data_nm=$1
+  shift
 
-    local rand_sd=$(printf "%04d" $1)
-    shift
+  local rand_sd=$(printf "%04d" $1)
+  shift
 
-    # TODO make this work with floating point numbers
-    local pct_lbl=$1
-    shift
+  # TODO make this work with floating point numbers
+  local pct_lbl=$1
+  shift
 
-    local learn_eval=$1
-    shift
+  local learn_eval=$1
+  shift
 
-    trap exit SIGINT
+  trap exit SIGINT
 
-    echo "data used: ${data_nm} | random seed: ${rand_sd} | percent labeled:${pct_lbl} | train test: ${learn_eval}"
+  echo "data used: ${data_nm} | random seed: ${rand_sd} | percent labeled:${pct_lbl} | train test: ${learn_eval}"
 
-    for exampleDir in "$@"; do
-        run_method "${exampleDir}" "${data_nm}" "${rand_sd}" "${pct_lbl}" "${learn_eval}" "${i}"
-    done
+  generate_data "${rand_sd}" "${data_name}" "eval"
+  for exampleDir in "$@"; do
+    run_method "${exampleDir}" "${data_nm}" "${rand_sd}" "${pct_lbl}" "${learn_eval}" "${i}"
+  done
 }
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
--- run_weight.sh
+++ run_weight.sh
@@ -6,25 +6,23 @@ readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly BASE_DATA_DIR="${THIS_DIR}/data"
 
 # An identifier to differentiate the output of this script/experiment from other scripts.
-readonly RUN_ID='run-all-weight-decoupled-smoothing'
+# readonly RUN_ID='run-all-weight-decoupled-smoothing'
 
 function generate_data() {
   random_seed=$1
   data_name=$2
   train_test=$3
 
-  echo "Generating data with seed ${random_seed} and data ${data_name} for ${train_test}"
-
   printf -v seed_nm "%04d" $random_seed
   local logPath="${BASE_DATA_DIR}/${train_test}/${data_name}/01pct/${seed_nm}rand/data_log.json"
-
-  echo "${logPath}"
 
   if [[ -e "${logPath}" ]]; then
     echo "Output data already exists, skipping data generation"
   elif [ "$train_test" = learn ]; then
+    echo "Generating data with seed ${random_seed} and data ${data_name} for ${train_test}"
     python3 write_psl_data.py --seed ${random_seed} --data ${data_name}.mat --learn
   else
+    echo "Generating data with seed ${random_seed} and data ${data_name} for ${train_test}"
     python3 write_psl_data.py --seed ${random_seed} --data ${data_name}.mat
   fi
 }
@@ -69,7 +67,7 @@ function main() {
 
       for rand_sd in 1 12345 837 2841 4293 6305 6746 9056 9241 9547; do
         echo "eval: Random ${rand_sd} | PCT ${pct_lbl} | method ${method}"
-        generate_data "${rand_seed}" "${data_name}" "eval"
+        generate_data "${rand_sd}" "${data_name}" "eval"
         ./run_method.sh "${data_name}" "${rand_sd}" "${pct_lbl}" "eval" "${method}"
       done
     done
